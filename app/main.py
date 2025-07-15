78d586dddc1faa420614e6d9e51b437dccddb8cd
Shrishvesh Reddy <vemareddy_sr@ee.iitr.ac.in>
2025-07-15 21:34:59 +0530

    feat: updated requirements,example.env main

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 # app/main.py
 from fastapi import FastAPI
-
+from app.api import interactions
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.include_router(interactions.router)
+
+origins = [
+    "http://localhost:5173",
+    "https://your-frontend-domain.com"
+]
+
+# Configure CORS middleware : change origins as per your frontend domain and during deployment
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Use ["*"] to allow all origins (not recommended for production)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def read_root():
     return {"message": "Hello, User!"}
